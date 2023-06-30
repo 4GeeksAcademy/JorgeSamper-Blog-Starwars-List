4af2bc306402b2de8ed425cccd2e253fb20653ac
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-30 13:10:55 +0000

    visaul styles

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
+
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center fw-bold" style={{bottom: 0, width: "100%"}}>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="https://www.linkedin.com/in/jorgesamperarias/">Jorge Samper</a>
 		</p>
 	</footer>
 );
